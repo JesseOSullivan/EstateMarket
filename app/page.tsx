caced35b61bf48fbd8f96f8f8a98ccb16859dc88
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2024-01-31 23:09:51 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function HomePage() {
           md:block  
           cursor-pointer
         "
-        fetchPriority='high'
 
         />
         {/* Responsive semi-transparent box with header text and search bar */}
