dd52694db78df75a03e7f5587b813d427242f630
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2024-02-10 21:27:14 +1000

    s

--- app/estates/page.tsx
+++ app/estates/page.tsx
@@ -1,7 +1,6 @@
 import { fetchLocation } from '@/app/lib/data';
 import EstatesPage from '@/app/ui/estates/EstatePage';
 import { fetchLocationByCoord, fetchSearchLocation } from '@/app/lib/data';
-import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
